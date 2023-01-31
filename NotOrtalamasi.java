f5b8a1acae892f8a6a055115aed8e9e1c0d22d32
mustfatsn <120778913+mustfatsn@users.noreply.github.com>
2023-01-31 17:35:07 +0300

    Add files via upload

--- NotOrtalamasi.java
+++ NotOrtalamasi.java
@@ -1,39 +1,41 @@
-package patika.dev;
+package deneme1;
 import java.util.Scanner;
-
 public class NotOrtalamasi {
 
 	public static void main(String[] args) {
-		// Değişkenleri oluştur
-		int mat, fizik, kimya, turkce, tarih, muzik;
+		int mat , turkce , kimya , fizik , muzik ;
 		
-		//Scanner sınıfını tanımladık
 		Scanner inp = new Scanner(System.in);
 		
-		// Kullanıcıdan değerleri al
-		System.out.print("Matematik Notunuz : ");
+		System.out.print("Matematik Notunuz :");
 		mat = inp.nextInt();
 		
-		System.out.print("Fizik Notunuz : ");
-		fizik = inp.nextInt();
-		
-		System.out.print("Kimya Notunuz : ");
-		kimya = inp.nextInt();
-		
-		System.out.print("Türkçe Notunuz : ");
+		System.out.print("Türkçe Notunuz :");
 		turkce = inp.nextInt();
 		
-		System.out.print("Müzik Notunuz : ");
+		System.out.print("Kimya Notunuz :");
+		kimya = inp.nextInt();
+		
+		System.out.print("Fizik Notunuz :");
+		fizik = inp.nextInt();
+		
+		System.out.print("Müzik Notunuz :");
 		muzik = inp.nextInt();
 		
-		System.out.print("Tarih Notunuz : ");
-	    tarih = inp.nextInt();
+		double avarage = (mat + turkce + kimya+ fizik + muzik) / 5 ;
 		
-		int toplam = ( mat + fizik + kimya + turkce + tarih + muzik );
-		double sonuc = toplam / 6.0 ;
-		System.out.println(sonuc);
-		
-		System.out.print(sonuc>60? "Ortalama= "+sonuc+" Sınıfı Geçti": "Ortalama= "+sonuc+" Sınıfta Kaldı" );
+		if ( (mat <=0 && mat <=100 ) && (turkce <=0 && turkce <=100) && (kimya <=0 && kimya <=100) 
+				&& (fizik <=0 && fizik <=100) && (muzik <=0 && muzik <=100) ) {
+			if (avarage >= 55) {
+				System.out.println("Tebrikler Sınıfı Geçtiniz");
+				System.out.println("Ortalamanız :" + avarage );
+			}
+			else {
+				System.out.println("Sınıfta Kaldınız, Seneye Görüşmek Üzere.");
+			}
+		}
+		else {
+			System.out.println("Notları 0-100 Arasında Giriniz.");
+		}
 	}
-
 }
